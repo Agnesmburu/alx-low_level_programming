aa852899a5f2cb6f574505d9ea91bc7413962468
Agnesmburu <wachekemburu97@gmail.com>
2023-10-30 17:12:37 -0700

    file I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -27,8 +27,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	lenr = read(fd, buffer, letters);
 	close(fd);
 	if (lenr == -1)
+	{
 		free(buffer);
 		return (0);
+	}
 	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
 	if (lenr != lenw)
